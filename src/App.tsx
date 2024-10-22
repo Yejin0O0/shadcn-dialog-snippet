4a01d386c4793a5dc4cc499bf936e1ea9472973e
iamgyu <iamgyu@naver.com>
2024-10-22 11:37:06 +0900

    refactor: index.css shadcn 기본과 맞추기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
+import { ThemeProvider } from "@/components/theme-provider";
 import { InnerScroll } from "./components/examples/InnerScroll";
+import { ModeToggle } from "./components/mode-toggle";
 
 function App() {
   return (
-    <>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <InnerScroll />
-    </>
+      <ModeToggle />
+    </ThemeProvider>
   );
 }
 

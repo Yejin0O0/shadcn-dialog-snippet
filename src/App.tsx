3ee153cb7527553ea2255590c658ca1d056cb7fb
Daeeui Kim <kimeodml@gmail.com>
2024-10-27 15:17:50 +0900

    feat: Header 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import Editor from "@monaco-editor/react";
-import { Button } from "./components/ui/button";
+import { Button } from "@/components/ui/button";
 import "@/App.css";
 import ErrorAlert from "@/components/examples/alert/ErrorAlert";
-import PaymentStep from "./components/examples/PaymentStep";
-import SignIn from "./components/examples/SignIn";
-import SignUp from "./components/examples/SignUp";
-import SurveyForm from "./components/examples/SurveyForm";
-import UserDetail from "./components/examples/UserDetail";
-import InfoAlert from "./components/examples/alert/InfoAlert";
-import SuccessAlert from "./components/examples/alert/SuccessAlert";
-import WarningAlert from "./components/examples/alert/WarningAlert";
+import PaymentStep from "@/components/examples/PaymentStep";
+import SignIn from "@/components/examples/SignIn";
+import SignUp from "@/components/examples/SignUp";
+import SurveyForm from "@/components/examples/SurveyForm";
+import UserDetail from "@/components/examples/UserDetail";
+import InfoAlert from "@/components/examples/alert/InfoAlert";
+import SuccessAlert from "@/components/examples/alert/SuccessAlert";
+import WarningAlert from "@/components/examples/alert/WarningAlert";
+import Header from "@/components/Header";
 
 function App() {
   return (
-    <main className="w-full">
+    <>
+      <Header />
+      <main className="w-full">
       <div className="contianer">
         <section className="mx-auto flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-10">
           <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">
@@ -48,6 +51,7 @@ function App() {
         </section>
       </div>
     </main>
+    </>
   );
 }
 

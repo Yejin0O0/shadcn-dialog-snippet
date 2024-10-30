26607e67800a23d2403d5bc4085eddeb572fdea5
Yejin <78341274+Yejin0O0@users.noreply.github.com>
2024-10-30 14:17:41 +0900

    design: grid layout 적용 (#38)

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import { Button } from "@/components/ui/button";
-import Editor from "@monaco-editor/react";
+import { Header } from "./components/Header";
+import { Button } from "./components/ui/button";
 import "@/App.css";
-import { InnerScrollForm } from "@/components/Form/InnerScrollForm";
-import { PaymentStepForm } from "@/components/Form/PaymentStepForm";
-import { SignInForm } from "@/components/Form/SignInForm";
-import { SignUpForm } from "@/components/Form/SignUpForm";
-import { SurveyForm } from "@/components/Form/SurveyForm";
-import { Header } from "@/components/Header";
-import { InfoDialog } from "@/components/Information/InfoDialog";
-import { SuccessAlert } from "@/components/Information/SuccessDialog";
-import { UserProfileDialog } from "@/components/Information/UserProfileDialog";
-import ErrorAlert from "@/components/Interrupt/Alert/ErrorAlert";
-import { WarningAlert } from "@/components/Interrupt/Alert/WarningAlert";
 
 function App() {
   return (
     <>
       <Header />
-      <main className="w-full">
-        <div className="contianer">
+      <main className="w-full flex-1">
+        <div className="container relative">
           <section className="mx-auto flex flex-col items-start gap-2 px-4 py-8 md:py-12 md:pb-8 lg:py-12 lg:pb-10">
             <h1 className="text-3xl font-bold leading-tight tracking-tighter md:text-4xl lg:leading-[1.1]">
               shadcn/ui Dialog Snippet
@@ -30,25 +19,19 @@ function App() {
               Documentation
             </Button>
           </section>
-          <section>
-            <div className="gap-6 md:flex md:flex-row-reverse md:items-start">
-              <div className="p-4 space-y-4 md:space-y-0 md:grid md:grid-cols-2 md:gap-4 lg:grid-cols-4">
-                <ErrorAlert />
-                <WarningAlert />
-                <InfoDialog />
-                <SuccessAlert />
-                <SignInForm />
-                <SurveyForm />
-                <SignUpForm />
-                <PaymentStepForm />
-                <UserProfileDialog />
-                <InnerScrollForm />
+          <section id="dialog" className="scroll-mt-20">
+            <div className="grid gap-4">
+              <div className="relative overflow-hidden max-w-[600px] lg:max-w-none">
+                tab 영역
               </div>
-              <Editor
-                height="90vh"
-                defaultLanguage="javascript"
-                defaultValue="// some comment"
-              />
+              <div className="gap-6 md:flex md:flex-row-reverse md:items-start">
+                <div
+                  id="category1"
+                  className="grid flex-1 scroll-mt-20 items-start gap-10 md:grid-cols-2 md:gap-6 lg:grid-cols-3 xl:gap-10"
+                >
+                  dialogCard
+                </div>
+              </div>
             </div>
           </section>
         </div>

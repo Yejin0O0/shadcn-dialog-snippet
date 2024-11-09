e8b72d23133c084393f90a0fc4958603eb102e96
Daeeui Kim <kimeodml@gmail.com>
2024-11-09 15:26:41 +0900

    refactor: use siteConfig, Icon

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,7 @@
-import GitHubIcon from "@/assets/github.svg";
-import LogoIcon from "@/assets/logo.svg";
-import TwitterIcon from "@/assets/twitter.svg";
-import { ModeToggle } from "./ModeToggle";
+import { Icons } from "@/components/common/Icons";
+import { ModeToggle } from "@/components/common/ModeToggle";
+import { Button } from "@/components/ui/button";
+import { siteConfig } from "@/config/site";
 
 export function Header() {
   return (
@@ -9,8 +9,10 @@ export function Header() {
       <div className="flex h-14 max-w-screen-2xl items-center justify-between px-8">
         <div className="mr-4 flex">
           <a className="mr-4 flex items-center space-x-2" href="/">
-            <LogoIcon />
-            <span className="font-bold lg:inline-block">shadcn/ui</span>
+            <Icons.logo className="h-6 w-6" />
+            <span className="hidden font-bold lg:inline-block">
+              {siteConfig.name}
+            </span>
           </a>
           <nav className="flex items-center gap-4 text-sm lg:gap-6">
             <a
@@ -22,24 +24,12 @@ export function Header() {
           </nav>
         </div>
         <nav className="flex items-center">
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/shadcn-ui/ui"
-            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground py-2 h-8 w-8 px-0"
-          >
-            <GitHubIcon />
-            <span className="sr-only">GitHub</span>
-          </a>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://twitter.com/shadcn"
-            className="inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 hover:bg-accent hover:text-accent-foreground py-2 h-8 w-8 px-0"
-          >
-            <TwitterIcon />
-            <span className="sr-only">Twitter</span>
-          </a>
+          <Button variant="ghost" size="icon" className="h-8 w-8 px-0">
+            <a target="_blank" rel="noreferrer" href={siteConfig.links.github}>
+              <Icons.gitHub />
+              <span className="sr-only">GitHub</span>
+            </a>
+          </Button>
           <ModeToggle />
         </nav>
       </div>

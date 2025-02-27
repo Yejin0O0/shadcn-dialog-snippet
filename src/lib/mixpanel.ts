48fc16391bcd1cf07e6f095f3c7730c86f0c9472
Yejin0O0 <yyj11kr@naver.com>
2025-02-27 22:08:36 +0900

    chore: change .env variable

--- src/lib/mixpanel.ts
+++ src/lib/mixpanel.ts
@@ -1,10 +1,17 @@
 import mixpanel from "mixpanel-browser";
 
-const MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_TOKEN;
+let MIXPANEL_TOKEN = "";
+
+if (import.meta.env.NODE_ENV === "production") {
+  MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_PROD_TOKEN;
+} else {
+  MIXPANEL_TOKEN = import.meta.env.VITE_MIXPANEL_DEV_TOKEN;
+}
 
 if (typeof window !== "undefined" && MIXPANEL_TOKEN) {
+  // 카테고리별로 이벤트 분기할 수 있으니 찾아볼 것
   mixpanel.init(MIXPANEL_TOKEN, {
-    debug: process.env.NODE_ENV !== "production",
+    debug: import.meta.env.NODE_ENV !== "production",
     track_pageview: false,
     persistence: "localStorage",
     // TODO: Apply Autocapture

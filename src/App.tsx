190eb2cdacea261a0aab85826fd20adb4cecf2a8
Yejin0O0 <yyj11kr@naver.com>
2025-03-13 14:57:57 +0900

    fix: fix tsc error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Layout from "@/layout/layout";
-import HelmetWrapper from "@/lib/helmetwrapper";
+import HelmetWrapper from "@/lib/helmetWrapper";
 import HomePage from "@/page/HomePage/HomePage";
 import { Route, Routes } from "react-router-dom";
 import "@/App.css";
